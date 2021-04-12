55418188dbed5f643b6637c103a0686614b83ae7
scm127 <67708429+scm127@users.noreply.github.com>
2021-04-12 17:04:16 -0400

    Update email_service.py

--- app/email_service.py
+++ app/email_service.py
@@ -14,7 +14,6 @@ SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
 def send_email(subject="[Daily Briefing] This is a test", html="<p>Hello World</p>", recipient_address=SENDER_EMAIL_ADDRESS):
     """
     Sends an email with the specified subject and html contents to the specified recipient,
-
     If recipient is not specified, sends to the admin's sender address by default.
     """
     client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
@@ -38,17 +37,14 @@ if __name__ == "__main__":
 
     example_html = f"""
     <h3>This is a test of the Daily Briefing Service</h3>
-
     <h4>Today's Date</h4>
     <p>Monday, January 1, 2040</p>
-
     <h4>My Stocks</h4>
     <ul>
         <li>MSFT | +3%</li>
         <li>GOOG | +2%</li>
         <li>AAPL | +4%</li>
     </ul>
-
     <h4>My Forecast</h4>
     <ul>
         <li>10:00 AM | 65 DEGREES | CLEAR SKIES</li>
